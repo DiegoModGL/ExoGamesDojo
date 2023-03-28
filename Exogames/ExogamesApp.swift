e522866abf9719d5b8d20c8030d21479408a9d52
Diego.monteagudo <diego.monteagudo@globant.com>
2023-03-28 09:41:33 -0500

    Initial Commit

---
+++ Exogames/ExogamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExogamesApp.swift
+//  Exogames
+//
+//  Created by Diego Sebastian Monteagudo Diaz on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ExogamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
